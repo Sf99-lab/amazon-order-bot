88cac6f807b02b9cbf97e0787d2dcfb3bc635882
Salman Farsi <sf991804@gmail.com>
2025-05-09 00:11:18 +0500

    Revert "testing script"
    This reverts commit a831f2f9b6f6aa7f0f45c2881df384ab22c7d55d.

--- server.mjs
+++ server.mjs
@@ -2,16 +2,16 @@ import cron from 'node-cron';
 import { runOrderAutomation } from './index.mjs';
 
 //Run at 9:00 AM EST daily 
-// cron.schedule('0 9 * * *', async () => {
-//   console.log(`[${new Date().toISOString()}] Running Amazon order task...`);
-//   await runOrderAutomation();
-// }, {
-//   timezone: 'America/New_York'
-// });
-
-cron.schedule('*/5 * * * *', async () => {
+cron.schedule('0 9 * * *', async () => {
   console.log(`[${new Date().toISOString()}] Running Amazon order task...`);
   await runOrderAutomation();
+}, {
+  timezone: 'America/New_York'
 });
+
+// cron.schedule('*/5 * * * *', async () => {
+//   console.log(`[${new Date().toISOString()}] Running Amazon order task...`);
+//   await runOrderAutomation();
+// });
 
 console.log('Scheduler initialized. Task will run daily at 9 AM EST.');
